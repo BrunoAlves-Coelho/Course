22f344e95186534cfd28358fcf3b8c24e26102a7
Bruno Alves <brunoacoelho86@gmail.com>
2021-03-26 00:21:12 -0300

    Inicio projeto Ordem de Pedido

---
+++ Course/Entities/Enums/OrderStatus.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace Course.Entities.Enums
+{
+    enum OrderStatus : int
+    {
+        //Pagto. Pendente
+        PendingPayment = 0,
+        //Processando
+        Processing = 1,
+        //Enviado
+        Shipped = 2,
+        //Entregue
+        Delivered = 3
+
+    }
+}
